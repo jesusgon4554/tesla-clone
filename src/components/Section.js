a5cf2fc933626fc78d493e547611661f738a5de8
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2021-05-30 14:27:28 -0700

    added home

---
+++ src/components/Section.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+    return (
+        <div>
+            <Container bgImage={""}>
+                <ItemText>
+                    <h1>Model S</h1>
+                    <p>Order Online for Touchless Delivery</p>
+                </ItemText>
+                <ButtonGroup>
+                    <LeftButton>Custom Order</LeftButton>
+                    <RightButton> Existing Inventory</RightButton>
+                </ButtonGroup>
+            </Container>
+        </div>
+    )
+}
+
+export default Section
+
+const Container = styled.div`
+background-color: lightblue;
+width:100%;
+height: 100vh;
+background-image: url('/images/model-s.jpg');
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const ItemText = styled.div`
+padding-top: 15vh;
+text-align: center;
+`
+
+const ButtonGroup = styled.div`
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    border-radius: 100px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    opacity: 0.85;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    color: black;
+    opacity: 0.65;
+`
